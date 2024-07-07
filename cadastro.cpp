187ebde4a14761c9812121fa544d7bbe688e4394
beatrizng <beatriz.n.alvares@gmail.com>
2024-07-07 18:28:49 -0300

    commit

--- cadastro.cpp
+++ cadastro.cpp
@@ -31,31 +31,20 @@ void Cadastro::salvarCadastro(){
 
 void Cadastro::cadastraJogador(const std::string& apelido, const std::string& nome) {
     if (jogadores.find(apelido) != jogadores.end()) {
-        std::cerr << "ERRO: jogador repetido" << std::endl;
+        std::cerr << "ERRO: dados incorretos" << std::endl;
         return;
     }
     jogadores[apelido] = Jogador(apelido, nome);
-    std::cout << "Jogador" << apelido << "cadastrado." << std::endl;
+    std::cout << "Jogador" << apelido << "cadastrado com sucesso" << std::endl;
 }
 
 void removeJogador(const std::string& apelido) {
     if(jogadores.erase(apelido)){
-        std::cout << "Jogador " << apelido << "removido. " << std::endl;
+        std::cout << "Jogador " << apelido << "removido com sucesso " << std::endl;
     } else {
-        std::cerr << "Jogador não cadastrado." << std::endl;
+        std::cerr << "ERRO: jogador inexistente" << std::endl;
     }
 }
 
-void Cadastro::listaJogadores() const {
-    for(const auto& [apelido, jogador] : jogadores){
-        std::cout << "Apelido: " << apelido << "Nome: " << jogador.nome << std::endl;
-    }
-}
-// 
-void Cadastro::salvarCadastro(){
-    std::ofstream arquivo("cadastro_jogadores.txt");
-
-    for(int &i : jogadores) {
-        arquivo << 
-    }
-}
+void Cadastro::listaJogadores(const std::string& ordem) const {
+    
