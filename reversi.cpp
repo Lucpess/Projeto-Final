91f608a8a7a72fcaaad92c469f5649d3fbb79070
Lucas Cordeiro <lutteribeiroo@gmail.com>
2024-08-17 08:49:16 -0300

    updates reversi

--- reversi.cpp
+++ reversi.cpp
@@ -22,13 +22,8 @@ void Reversi::anunciaTurno(std::string jogador1, std::string jogador2){
     }
 }
 
-std::string Reversi::corOposta() {
-    if("PRETO" == turno) {
-        return "BRANCO";
-    } else {
-        return "PRETO";
-    }
-    return "NULO";
+std::string Reversi::pecaAdversaria() {
+    return (turno == "PRETO") ? "BRANCO" : "PRETO";
 }
 
 bool Reversi::posicaoPermitida(int i, int j, std::string turno) {
@@ -41,17 +36,16 @@ bool Reversi::posicaoPermitida(int i, int j, std::string turno) {
     else return false;
 }
 
-bool Reversi::jogadaPermitida(int linha, int col) const {
-    if (tabuleiro.verificaCasa(linha, col, "NULO"))
-    return true;
+bool Reversi::jogadaPermitida(int linha, int col) {
+    return tabuleiro.verificaCasa(linha, col, "NULO") && posicaoPermitida(linha, col, turno);
 }
 
 bool Reversi::confereColuna(int i, int j, std::string turno) {
     if (tabuleiro.verificaCasa(i, j, "NULO")) {
         if ( i >= 2) { // confere coluna para cima
-            if (tabuleiro.verificaCasa(i-1, j, corOposta())) {
+            if (tabuleiro.verificaCasa(i-1, j, pecaAdversaria())) {
                 for ( int x = i - 2; x >= 0 ; --x) {
-                    if(tabuleiro.verificaCasa(x, j, corOposta()))
+                    if(tabuleiro.verificaCasa(x, j, pecaAdversaria()))
                         continue;
 
                     if(tabuleiro.verificaCasa(x, j, turno)) 
@@ -63,9 +57,9 @@ bool Reversi::confereColuna(int i, int j, std::string turno) {
             } 
         }
         if (i <= 5) { // confere coluna para baixo
-            if (tabuleiro.verificaCasa(i+1, j, corOposta())) {
+            if (tabuleiro.verificaCasa(i+1, j, pecaAdversaria())) {
                 for ( int x = i+2; x < 8; ++x) {
-                    if (tabuleiro.verificaCasa(x, j, corOposta())) 
+                    if (tabuleiro.verificaCasa(x, j, pecaAdversaria())) 
                         continue;
 
                     if(tabuleiro.verificaCasa(x, j, turno))
@@ -83,9 +77,9 @@ bool Reversi::confereColuna(int i, int j, std::string turno) {
 bool Reversi::confereLinha(int i, int j, std::string turno) {
     if (tabuleiro.verificaCasa(i, j, "NULO")) {
         if (j >=2) {// confere linha p esquerda
-            if (tabuleiro.verificaCasa(i, j - 1, corOposta())) {
+            if (tabuleiro.verificaCasa(i, j - 1, pecaAdversaria())) {
                 for(int y = j - 2; y>=0; --y) {
-                    if(tabuleiro.verificaCasa(i, y, corOposta()))
+                    if(tabuleiro.verificaCasa(i, y, pecaAdversaria()))
                         continue;
                     if(tabuleiro.verificaCasa(i, y, turno))
                         return true;
@@ -95,9 +89,9 @@ bool Reversi::confereLinha(int i, int j, std::string turno) {
             }
         }
         if (j <= 5) {// confere linha p direita
-            if(tabuleiro.verificaCasa(i, j+1, corOposta())) {
+            if(tabuleiro.verificaCasa(i, j+1, pecaAdversaria())) {
                 for (int y = j+2; y < 8; ++y) {
-                    if(tabuleiro.verificaCasa(i, y, corOposta()))
+                    if(tabuleiro.verificaCasa(i, y, pecaAdversaria()))
                         continue;
                     if(tabuleiro.verificaCasa(i, y, turno))
                         return true;
@@ -113,9 +107,9 @@ bool Reversi::confereLinha(int i, int j, std::string turno) {
 bool Reversi::confereDiagonal(int i, int j, std::string turno) {
     if (tabuleiro.verificaCasa( i, j, "NULO")) {
         if (i >= 2 && j >= 2 ) { // confere diagonal p/ esquerda e p/ cima
-            if (tabuleiro.verificaCasa(i-1, j-1, corOposta())) {
+            if (tabuleiro.verificaCasa(i-1, j-1, pecaAdversaria())) {
                 for (int x = i-2, y = j-2; x >=0 && y>=0; --x, --y ) {
-                    if (tabuleiro.verificaCasa(x, y, corOposta()))
+                    if (tabuleiro.verificaCasa(x, y, pecaAdversaria()))
                         continue;
 
                     if(tabuleiro.verificaCasa(x, y, turno))
@@ -127,9 +121,9 @@ bool Reversi::confereDiagonal(int i, int j, std::string turno) {
             }
         }
         if (i <=5 && j >= 2) { // confere diagonal p/ esquerda e p/ baixo
-            if (tabuleiro.verificaCasa(i+1, j-1, corOposta())) {
+            if (tabuleiro.verificaCasa(i+1, j-1, pecaAdversaria())) {
                 for (int x = i+2, y = j-2; x < 8 && y >=0; ++x, ++y) {
-                    if (tabuleiro.verificaCasa(x, y, corOposta()))
+                    if (tabuleiro.verificaCasa(x, y, pecaAdversaria()))
                         continue;
 
                     if (tabuleiro.verificaCasa(x, y, turno))
@@ -141,9 +135,9 @@ bool Reversi::confereDiagonal(int i, int j, std::string turno) {
             }
         }
         if (i >=2 && j <= 5) { // confere diagonal p/ direita e p/ cima
-            if(tabuleiro.verificaCasa(i-1, j+1, corOposta())) {
+            if(tabuleiro.verificaCasa(i-1, j+1, pecaAdversaria())) {
                 for (int x = i-2, y = j+2; x >= 0 && y < 8; --x, ++y) {
-                    if(tabuleiro.verificaCasa(x, y, corOposta()))
+                    if(tabuleiro.verificaCasa(x, y, pecaAdversaria()))
                         continue;
 
                     if(tabuleiro.verificaCasa(x, y, turno))
@@ -157,7 +151,7 @@ bool Reversi::confereDiagonal(int i, int j, std::string turno) {
         if(i <=5 && j <=5) {// confere diagonal p/ direita e p/ baixo
             if(tabuleiro.verificaCasa(i+1, j+1, "NULO")) {
                 for(int x = i+2, y = j+2; x < 8 && y < 8; ++x, ++y) {
-                    if (tabuleiro.verificaCasa(x, y, corOposta()))
+                    if (tabuleiro.verificaCasa(x, y, pecaAdversaria()))
                         continue;
 
                     if(tabuleiro.verificaCasa(x, y, turno))
@@ -172,32 +166,19 @@ bool Reversi::confereDiagonal(int i, int j, std::string turno) {
     return false;
 }
 
-void Reversi::jogadasValidas() {
-    jogadasLinha.clear();
-    jogadasColuna.clear();
-
-    for(int i = 0; i < 8; i++) {
-        for(int j = 0; j < 8; j++) {
-            if(posicaoPermitida(i, j, turno)) {
-            jogadasLinha.push_back(i);
-            jogadasColuna.push_back(j);
-            std::cout << "[" << i << "," << j << "]" << std::endl;
-            }
-        }
-    }
-}
-vector<vector<string>> Reversi::viraPecasVertical(int i, int j, string turno){
+void Reversi::viraPecasColuna(int i, int j, string turno){
     int x;
 
     if(i >= 2) {//vira as peças na vertical pra cima
-        if(tabuleiro.verificaCasa(i-1, j, corOposta())) {
+        if(tabuleiro.verificaCasa(i-1, j, pecaAdversaria())) {
             for(x = i-2; x>= 0; --x) {
-                if (tabuleiro.verificaCasa(x, j, corOposta()))
+                if (tabuleiro.verificaCasa(x, j, pecaAdversaria()))
                 continue;
 
                 if (tabuleiro.verificaCasa(x, j, turno)){
-                    for (int k= i-1; k>x; --k)
-                    tabuleiro.modificaTabuleiro(k, j, turno);
+                    for (int k= i-1; k>x; --k) {
+                        tabuleiro.modificaTabuleiro(k, j, turno);
+                    }
                     break;
                 }
 
@@ -207,14 +188,15 @@ vector<vector<string>> Reversi::viraPecasVertical(int i, int j, string turno){
         }
     }
     if (i <= 5) {//vira as peças na vertical p/ baixo
-        if(tabuleiro.verificaCasa(i+1, j, corOposta())) {
+        if(tabuleiro.verificaCasa(i+1, j, pecaAdversaria())) {
             for(x = i+2; x<8;++x) {
-                if(tabuleiro.verificaCasa(x, j, corOposta()))
+                if(tabuleiro.verificaCasa(x, j, pecaAdversaria()))
                     continue;
 
                 if (tabuleiro.verificaCasa(x, j, turno)) {
-                    for(int k = i+1; k<x; ++k)
+                    for(int k = i+1; k<x; ++k) {
                     tabuleiro.modificaTabuleiro(k, j, turno);
+                    }
                     break;
                 }
 
@@ -223,22 +205,22 @@ vector<vector<string>> Reversi::viraPecasVertical(int i, int j, string turno){
             }
         }
     }
-    printTabuleiro();
 }
 
-vector<vector<string>> Reversi::viraPecasHorizontal(int i, int j, string turno) {
+void Reversi::viraPecasLinha(int i, int j, string turno) {
     int y;
 
     if(j >= 2) {//vira as peças na horizontal p/ esquerda
-       if (tabuleiro.verificaCasa(i, j-1, corOposta())) {
+       if (tabuleiro.verificaCasa(i, j-1, pecaAdversaria())) {
             for( y = j-2; y >=0; --y) {
-                if (tabuleiro.verificaCasa(i, y, corOposta())) 
+                if (tabuleiro.verificaCasa(i, y, pecaAdversaria())) 
                     continue;
 
                 if (tabuleiro.verificaCasa(i, y, turno)) {
-                    for (int k = j-1; k > y; --k)
+                    for (int k = j-1; k > y; --k) {
                         tabuleiro.modificaTabuleiro(i, k, turno);
-                        break;
+                    }
+                    break;
                 }
 
                 if (tabuleiro.verificaCasa(i, y, "NULO"))
@@ -247,37 +229,38 @@ vector<vector<string>> Reversi::viraPecasHorizontal(int i, int j, string turno) 
         }
     }
     if(j <=5) {
-        if (tabuleiro.verificaCasa(i, j+1, corOposta())) {
+        if (tabuleiro.verificaCasa(i, j+1, pecaAdversaria())) {
             for(y = j+2; y < 8; ++y) {
-                if (tabuleiro.verificaCasa(i, y, corOposta()))
+                if (tabuleiro.verificaCasa(i, y, pecaAdversaria()))
                     continue;
 
                 if (tabuleiro.verificaCasa(i, y, turno)) {
-                    for (int k = j+1; k<y; ++k)
+                    for (int k = j+1; k<y; ++k) {
                         tabuleiro.modificaTabuleiro(i, k, turno);
-                        break;
+                    }
+                    break;
                 }
                 if (tabuleiro.verificaCasa(i, y, "NULO"))
                     break;
             }
         }
     }
-    printTabuleiro();
 }
 
-vector<vector<string>> Reversi::viraPecasDiagonal(int i, int j, string turno) {
+void Reversi::viraPecasDiagonal(int i, int j, string turno) {
     int x, y;
 
     if (i >= 2 && j >= 2) {//vira as peças em cima e para a esq.
-        if (tabuleiro.verificaCasa(i-1, j-1, corOposta())) {
+        if (tabuleiro.verificaCasa(i-1, j-1, pecaAdversaria())) {
             for (x=i-2,y=j-2;x>=0 && y>=0;--x,--y) {
-                if (tabuleiro.verificaCasa(x, y, corOposta()))
+                if (tabuleiro.verificaCasa(x, y, pecaAdversaria()))
                     continue;
 
                 if (tabuleiro.verificaCasa(x, y, turno)) {
-                    for(int k=i - 1, l=j - 1;k>x && l>y;--k,--l)
+                    for(int k=i - 1, l=j - 1;k>x && l>y;--k,--l) {
                         tabuleiro.modificaTabuleiro(k, l, turno);
-                        break;
+                    }
+                    break;
                 }
                 if (tabuleiro.verificaCasa(x, y, "NULO"))
                     break;
@@ -285,15 +268,16 @@ vector<vector<string>> Reversi::viraPecasDiagonal(int i, int j, string turno) {
         }
     }
     if(i <=5 && j >= 2) {//vira as peças em baixo e para a esq.
-        if(tabuleiro.verificaCasa(i+1, j-1, corOposta())) {
+        if(tabuleiro.verificaCasa(i+1, j-1, pecaAdversaria())) {
             for(x=i+2, y=j-2; x<8 && y >=0;++x,--y) {
-                if (tabuleiro.verificaCasa(x, y, corOposta()))
+                if (tabuleiro.verificaCasa(x, y, pecaAdversaria()))
                     continue;
 
                 if (tabuleiro.verificaCasa(x, y, turno)) {
-                    for(int k=i+1, l=j-1; k<x && l>y; ++k,--l)
+                    for(int k=i+1, l=j-1; k<x && l>y; ++k,--l) {
                         tabuleiro.modificaTabuleiro(k, l, turno);
-                        break;
+                    }
+                    break;
                 }
                 if (tabuleiro.verificaCasa(x, y, "NULO"))
                     break;
@@ -301,13 +285,14 @@ vector<vector<string>> Reversi::viraPecasDiagonal(int i, int j, string turno) {
         }
     }
     if(i >=2 && j <= 5) {//vira as peças em cima e para a direita
-        if(tabuleiro.verificaCasa(i-1,j+1, corOposta())) {
-            for(x = i-2, y=j+2; x>=0 && y <= 7; --x, ++y) {
-                if(tabuleiro.verificaCasa(x, y, corOposta()))
+        if(tabuleiro.verificaCasa(i-1,j+1, pecaAdversaria())) {
+            for(x = i-2, y= j+2; x>=0 && y <= 7; --x, ++y) {
+                if(tabuleiro.verificaCasa(x, y, pecaAdversaria()))
                     continue;
                 if(tabuleiro.verificaCasa(x,y,turno)) {
-                    for(int k= i-1, l= j+1;k>x && l<y;--k,++l)
-                    tabuleiro.modificaTabuleiro(k, l, turno);
+                    for(int k= i-1, l= j+1;k>x && l<y;--k,++l) {
+                        tabuleiro.modificaTabuleiro(k, l, turno);
+                    }
                     break; 
                 }
                 if(tabuleiro.verificaCasa(x, y, "NULO"))
@@ -316,13 +301,14 @@ vector<vector<string>> Reversi::viraPecasDiagonal(int i, int j, string turno) {
         }
     }
     if( i<=5 && j<=5) {//vira as peças em baixo e para a direita
-        if(tabuleiro.verificaCasa(i+1, j+1, corOposta())){
-            for(x=i+2,y=j+2;x<8 && y<8; ++x,++y) {
-                if(tabuleiro.verificaCasa(x, y, corOposta()))
+        if(tabuleiro.verificaCasa(i+1, j+1, pecaAdversaria())){
+            for(x= i+2, y= j+2; x < 8 && y < 8; ++x,++y) {
+                if(tabuleiro.verificaCasa(x, y, pecaAdversaria()))
                     continue;
                 if(tabuleiro.verificaCasa(x, y, turno)) {
-                    for(int k=i+1,l=j+1; k<x && l<y;++k,++l)
-                    tabuleiro.modificaTabuleiro(k, l, turno);
+                    for(int k=i+1,l=j+1; k<x && l<y;++k,++l) {
+                        tabuleiro.modificaTabuleiro(k, l, turno);
+                    }
                     break;
                 }
                 if(tabuleiro.verificaCasa(x, y, "NULO"))
@@ -330,22 +316,44 @@ vector<vector<string>> Reversi::viraPecasDiagonal(int i, int j, string turno) {
             }
         }
     }
-    printTabuleiro();
 }
 
-bool Reversi::verificaFimDeJogo() const {
+void Reversi::jogadorAtual(std::string jogador1, std::string jogador2) {
+    if (turno == "PRETO") {
+        jogador = jogador1;
+    }
+    else jogador = jogador2;
+}
 
-    bool tabuleiroCheio = true;
+bool Reversi::temJogadasValidas(std::string turno) {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (posicaoPermitida(i, j, turno)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+    bool Reversi::verificaFimDeJogo() {
     int pecasBrancas, pecasPretas;
     contarPecas(pecasBrancas, pecasPretas);
-    int totalPecas = pecasBrancas + pecasPretas;
+    bool tabuleiroCheio = true;
 
-    while(tabuleiroCheio) {
+    // Verifica se há espaço no tabuleiro para jogar
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (posicaoPermitida(i, j, "PRETO") || posicaoPermitida(i, j, "BRANCO")) {
+                tabuleiroCheio = false;
+                break;
+            }
+        }
+        if (!tabuleiroCheio) break;
+    }
 
-        if (totalPecas < 64 ) {
-            tabuleiroCheio = false;
-            break;
-        } else if (pecasBrancas > pecasPretas) {
+    if (tabuleiroCheio) {
+        if (pecasBrancas > pecasPretas) {
             std::cout << "BRANCAS VENCERAM!" << std::endl;
         } else if (pecasPretas > pecasBrancas) {
             std::cout << "PRETAS VENCERAM!" << std::endl;
@@ -353,29 +361,14 @@ bool Reversi::verificaFimDeJogo() const {
             std::cout << "EMPATE. TABULEIRO CHEIO!" << std::endl;
         }
         return true;
+    } else if (pecasBrancas == 0) {
+        std::cout << "AS BRANCAS NÃO TEM MAIS PEÇAS. PRETAS VENCERAM!" << std::endl;
+        return true;
+    } else if (pecasPretas == 0) {
+        std::cout << "AS PRETAS NÃO TEM MAIS PEÇAS. BRANCAS VENCERAM!" << std::endl;
+        return true;
     }
 
-    if(tabuleiroCheio) { 
-        if (pecasBrancas > pecasPretas) {
-            std::cout << "BRANCAS VENCERAM!" << std::endl;
-        }
-        else if (pecasPretas > pecasBrancas) {
-            std::cout << "PRETAS VENCERAM!" << std::endl;
-        }
-        else {
-            std::cout << "EMPATE. TABULEIRO CHEIO!" << std::endl;
-        }
-        return true;
-    }
-    else { 
-        if (pecasBrancas == 0) {
-            std::cout << "AS BRANCAS NÃO TEM MAIS PEÇAS.PRETAS VENCERAM!" << std::endl;
-        }
-        else if (pecasPretas == 0) {
-            std::cout << "AS PRETAS NÃO TEM MAIS PEÇAS.BRANCAS VENCERAM!" << std::endl;
-        }
-        return true;
-    }
     return false;
 }
 
@@ -387,15 +380,15 @@ void Reversi::mudaTurno(){
     }
 }
 
-void Reversi::contarPecas(int& brancas, int& pretas) const{
-    brancas = 0;
-    pretas = 0;
+void Reversi::contarPecas(int& pecasBrancas, int& pecasPretas) {
+    pecasBrancas = 0;
+    pecasPretas = 0;
     for(int i = 0; i < 8; i++) {
         for(int j = 0; j < 8; j++) {
-            if (casa.estado == "BRANCO") {
-                brancas++;
-            } else if (casa.estado == "PRETO") {
-                pretas++;
+            if (tabuleiro.verificaCasa(i, j, "BRANCO")) {
+                pecasBrancas++;
+            } else if (tabuleiro.verificaCasa(i, j, "PRETO")) {
+                pecasPretas++;
             }
         }
     }
@@ -406,8 +399,15 @@ void Reversi::start(std::string jogador1, std::string jogador2) {
     printTabuleiro();
     std::cout << jogador1 << " == X | " << jogador2 << " == O" << std::endl;
     while(!fimDeJogo){
-        int linha;
-        int coluna;
+        int linha =-1;
+        int coluna =-1;
+        
+        if (!temJogadasValidas(turno)) {
+            std::cout << "SEM JOGADAS VÁLIDAS PARA " << (turno == "PRETO" ? jogador1 : jogador2) << ". VEZ DE " << (turno == "PRETO" ? jogador2 : jogador1) << "." << std::endl;
+            mudaTurno();
+            continue;
+        }
+
         while (true) {
                 anunciaTurno(jogador1, jogador2);
                 if (!(std::cin >> linha >> coluna)) {
@@ -422,12 +422,13 @@ void Reversi::start(std::string jogador1, std::string jogador2) {
                     break;  // Sai do loop se a entrada for válida
                 }
         }
-        posicaoPermitida(linha, coluna, turno);
+        
         tabuleiro.modificaTabuleiro(linha, coluna, turno);
-        viraPecasVertical(linha, coluna, turno);
-        viraPecasHorizontal(linha, coluna, turno);
+        viraPecasColuna(linha, coluna, turno);
+        viraPecasLinha(linha, coluna, turno);
         viraPecasDiagonal(linha, coluna, turno);
-        verificaFimDeJogo();
-        mudaTurno();
+        fimDeJogo = verificaFimDeJogo();
+        if(!fimDeJogo)
+            mudaTurno();
     } 
 }
--- reversi.hpp
+++ reversi.hpp
@@ -2,39 +2,36 @@
    #define REVERSI_HPP
 
    #include "tabuleiro.hpp"
-   using namespace std;
-
+   #include "cadastro.hpp"
    class Reversi {
    public:
       Reversi();
       void start(std::string jogador1, std::string jogador2 );
-      bool escolheJogada(int linha, int col);
-      bool verificaFimDeJogo() const;
+      bool verificaFimDeJogo();
       void printTabuleiro() const;
-      void listarJogadasValidas();
+      void jogadorAtual(std::string jogador1, std::string jogador2);
       bool confereLinha(int, int, std::string estado);
       bool confereColuna(int, int, std::string estado);
       bool confereDiagonal(int, int, std::string estado);
-      void contarPecas(int& brancas, int& pretas);
-      std::string corOposta();
+      void contarPecas(int& pecasBrancas, int& pecasPretas);
+      std::string pecaAdversaria();
       bool posicaoPermitida(int i, int j, std::string turno);
-      void jogadasValidas();
+      bool temJogadasValidas(std::string jogador);
 
    private:
       bool fimDeJogo;
       Tabuleiro tabuleiro;
-      Casa casa;
       std::string turno;
-      bool jogadaPermitida(int linha, int col) const;
-      void novaJogada(int linha, int col);
-      vector<vector<string>> viraPecasVertical(int linha, int col, string turno);
-      vector<vector<string>> viraPecasHorizontal(int linha, int col, string turno);
-      vector<vector<string>> viraPecasDiagonal(int linha, int col, string turno);
+      bool jogadaPermitida(int linha, int col);
+      void viraPecasColuna(int linha, int col, std::string turno);
+      void viraPecasLinha(int linha, int col, std::string turno);
+      void viraPecasDiagonal(int linha, int col, std::string turno);
       void mudaTurno();
       void anunciaTurno(std::string jogador1, std::string jogador2);
       void adicionaPeca(int linha, int col);
       std::vector<int> jogadasLinha;
       std::vector<int> jogadasColuna;
+      std::string jogador;
    };
 
    #endif 
